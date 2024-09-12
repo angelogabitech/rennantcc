b1d13a75099a5ffe3bc57fce789809fabef980fa
Angelo Gabriel <168835656+angelogabitech@users.noreply.github.com>
2024-09-11 22:19:02 -0300

    crud eventos

---
+++ eventosBD.py
@@ -0,0 +1,71 @@
+import mysql.connector
+
+def iniciaConexao():
+    conexaoBanco = mysql.connector.connect(
+      host="127.0.0.1",
+      user="root",
+      password="protasete00",
+      database="protasete",
+      client_flags=[mysql.connector.ClientFlag.PLUGIN_AUTH]
+    )
+    return conexaoBanco
+
+
+def createEvento(nome, imagem, descricao, dias): 
+    conexao = iniciaConexao()
+    cursor = conexao.cursor()
+    comando = "INSERT INTO Evento (nome, imagem, descriçao, dias) VALUES(%s, %s, %s, %s)"
+    valores = [nome, imagem, descricao, dias]
+    cursor.execute(comando, valores)
+    conexao.commit() # edita o banco de dados
+    #resultado= cursor.fetchtall() #ler o banco de dados
+    print("Evento cadastrado com sucesso")
+    cursor.close()
+    conexao.close()
+
+def deleteEvento(id):    
+    conexao = iniciaConexao()
+    cursor = conexao.cursor()
+    comando = "DELETE FROM evento WHERE id = %s"
+    valores = [id]
+    cursor.execute(comando, valores)
+    conexao.commit() # edita o banco de dados
+    #resultado= cursor.fetchtall() #ler o banco de dados
+    print("Evento excluido com sucesso")
+    cursor.close()
+    conexao.close()
+
+
+def updateEvento(nome,imagem,descricao,dias,id):
+     conexao = iniciaConexao()
+     cursor = conexao.cursor()
+     comando = "UPDATE evento SET nome=%s,imagem = %s,dias = %s, descricao = %s WHERE id= %i"
+     valores = [nome,imagem,descricao,dias,id]
+     cursor.execute(comando, valores)
+     conexao.commit() # edita o banco de dados
+        #resultado= cursor.fetchtall() #ler o banco de dados
+     print("Evento atualizado com sucesso")
+     cursor.close()
+     conexao.close()
+
+
+def readcardapio():
+    conexao = iniciaConexao()
+    cursor = conexao.cursor()
+    comando = "SELECT * FROM evento;"
+    cursor.execute(comando)
+    eventos = cursor.fetchall()
+    eventos_list = []
+    for evento in eventos:
+        eventos_list.append({
+            'nome': evento[1],
+            'imagem': evento[2],
+            'descricao': evento[3],
+            'dias': evento[4]
+        })
+        cursor.close()
+        conexao.close()
+    return eventos_list
+    conexao.commit() # edita o banco de dados
+    #resultado= cursor.fetchtall() #ler o banco de dados
+    print("Evento")
